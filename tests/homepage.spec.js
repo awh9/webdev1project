9f58d577a339e20c090f20a8ce7deecb06f7f839
awh9 <113126029+awh9@users.noreply.github.com>
2023-10-31 13:51:08 -0400

    ensures that UTF-8 meta tag & hero h1 is present

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,14 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
-test('check that "express works" text is present', async ({ page }) => {
-  await page.goto('/');
-  
-  // Using the expect method with a page locator
-  // This will check if the text "express works" is present anywhere on the page
-  // Using a locator to get the text content of the h1 element
-  const heading = page.locator('h1');
-  await expect(heading).toHaveText('express works');
+test('check that "Andre Henry" h1 is present', async ({ page }) => {
+  // Using a locator to get the h1 element with the text "Andre Henry"
 
+  await page.goto('/');
+  const heading = page.locator('h1:has-text("Andre Henry")');
+  await expect(heading).toBeVisible();
 });
 
 test('check that UTF-8 meta tag is present', async ({ page }) => {
@@ -18,6 +15,6 @@ test('check that UTF-8 meta tag is present', async ({ page }) => {
   //Act: Get the content attribute of the meta charset tag
   const metaCharset = await page.$eval('meta[charset]', (meta) => meta.getAttribute('charset'));
 
-  //Assert: Check if the charset is set to UTF-8
+  //Assert: Check if the charset is set to utf-8
   await expect(metaCharset).toBe('utf-8');
 });
