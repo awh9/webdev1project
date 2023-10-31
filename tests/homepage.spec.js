fa39f0d84e868be7a840ee4acfdd195c91bdf11a
awh9 <113126029+awh9@users.noreply.github.com>
2023-10-31 14:06:20 -0400

    test to check that the Project section is present

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,13 +1,5 @@
 const { test, expect } = require('@playwright/test');
 
-test('check that "Andre Henry" h1 is present', async ({ page }) => {
-  // Using a locator to get the h1 element with the text "Andre Henry"
-
-  await page.goto('/');
-  const heading = page.locator('h1:has-text("Andre Henry")');
-  await expect(heading).toBeVisible();
-});
-
 test('check that UTF-8 meta tag is present', async ({ page }) => {
   //Arrange: Go to the site homepage
   await page.goto('/');
@@ -18,3 +10,20 @@ test('check that UTF-8 meta tag is present', async ({ page }) => {
   //Assert: Check if the charset is set to utf-8
   await expect(metaCharset).toBe('utf-8');
 });
+
+test('check that "Andre Henry" h1 is present', async ({ page }) => {
+  // Using a locator to get the h1 element with the text "Andre Henry"
+
+  await page.goto('/');
+  const heading = page.locator('h1:has-text("Andre Henry")');
+  await expect(heading).toBeVisible();
+});
+
+test('check that the "Projects" section is present', async ({ page }) => {
+  // Using a locator to get the section element with the text "Projects"
+
+  await page.goto('/');
+  const heading = page.locator('h1:has-text("Projects")');
+  await expect(heading).toBeVisible();
+}
+);
